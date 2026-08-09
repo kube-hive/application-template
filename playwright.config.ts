faa581b68ae84843fbe48b50387f0cb8e639e851
axel7083 <42176370+axel7083@users.noreply.github.com>
2026-08-09 10:53:51 +0200

    fix: e2e
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,23 @@
 import { defineConfig } from '@playwright/test';
+import type {LaunchOptions} from "playwright-core";
+
+function getLaunchOptions(): Omit<LaunchOptions, 'tracesDir'> | undefined {
+    if(!process.env.PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH ) {
+        return undefined;
+    }
+
+    return {
+        executablePath: process.env.PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }
+}
 
 export default defineConfig({
     // Look for test files in the "tests" directory, relative to this configuration file.
     testDir: 'tests/playwright',
     // Configure Playwright to use the container's native Fedora Chromium package
     use: {
-        launchOptions: {
-            executablePath: '/usr/bin/chromium-browser',
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
-        }
+        launchOptions: getLaunchOptions(),
     },
     // Run your local dev server before starting the tests.
     webServer: {
