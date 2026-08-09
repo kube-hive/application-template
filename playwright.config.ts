1e083a97ad884540588d97ff773f1baa14b7e4bd
axel7083 <42176370+axel7083@users.noreply.github.com>
2026-08-09 10:46:54 +0200

    fix: configure Playwright to use chromium package
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,13 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
     // Look for test files in the "tests" directory, relative to this configuration file.
     testDir: 'tests/playwright',
+    // Configure Playwright to use the container's native Fedora Chromium package
+    use: {
+        launchOptions: {
+            executablePath: '/usr/bin/chromium',
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        }
+    },
     // Run your local dev server before starting the tests.
     webServer: {
         command: 'npm run build && npm run preview',
